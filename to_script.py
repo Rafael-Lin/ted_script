e4c4d068ebfde40f7a84370a34e1ece9026806ec
Rafael Lin <rlin@tw-rafaellin.synaptics-inc.local>
2015-04-29 12:55:21 +0800

    Could recognize the parameter1 as a folder to handle

--- to_script.py
+++ to_script.py
@@ -2,10 +2,18 @@
 import os
 import sys
 import re
-def function(path) :
 
-    output_name, tmp = path.split('_') 
+def transform(path, current) :
+
+    if 'spa' in path :
+	return
+
+    tmp_list = path.split('_') 
+    tmp_str = tmp_list[0]
+    tmp_list_1 = tmp_str.split('\\')
+    output_name = tmp_list_1[-1]
     output_name += '.txt'
+    output_name = os.path.join( current , output_name )
     lines = open(path, 'r').readlines()
     file = open(output_name, "w")
     for line in lines:
@@ -19,9 +27,20 @@ def function(path) :
     print 'success! transform to ' + output_name
     file.close()
 def main() :
+    current_folder = os.getcwd()
+    print current_folder
 
     if (len(sys.argv) == 2) : 
-	function( sys.argv[1] ) 
+	if os.path.isdir( sys.argv[1] ) :
+	    for root,dirs, files in os.walk( sys.argv[1] ) :
+		for f in files :
+		    # print root
+		    # print dirs
+		    # print f
+		    target = os.path.join( root , f )
+		    transform( target , current_folder ) 
+	else :
+	    transform( sys.argv[1], current_folder ) 
     else :
 	print "please enter the path"
 if __name__ == '__main__':
